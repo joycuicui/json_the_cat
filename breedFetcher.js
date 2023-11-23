c6b8b5100a9196a912ff78b34cba599ffde8215f
joycuicui <cuiqijoy@outlook.com>
2023-11-23 15:49:42 -0800

    add index and test files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,21 @@
 const request = require("request");
-const breed = process.argv[2];
-const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
 
-request(url, (error, response, body) => {
-  if (body === "[]") {
-    console.log("Error: breed not found.");
-  } else if (error) {
-    console.log("Error: ", error);
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+fetchBreedDescription = function (breedName, callback) {
+  const url = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    if (!error) {
+      if (body === "[]") {
+        callback("breed not found.", null);
+      } else {
+        const data = JSON.parse(body);
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
